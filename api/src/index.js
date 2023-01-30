7a72516bb842ebac8ef34f9d8ec0ffff311f787c
FeDe <federampi@gmail.com>
2023-01-30 19:26:52 -0300

    acces control

--- api/src/index.js
+++ api/src/index.js
@@ -25,6 +25,20 @@ app.use(morgan('dev'));
 
 //metodo que sirve recibir los datos desde el cliente 
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+
+
 //Routes
 const routes = require("./routes/index.js");
 app.use("/", routes);
